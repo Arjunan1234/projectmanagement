47fb8dc5f2e7c91b616107bf629bcac90a38145d
esear <arjunanvijay21@gmail.com>
2024-01-27 08:47:42 +0530

    deploy config change

--- pizarra/settings.py
+++ pizarra/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'pizarra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
          'NAME':'railway',
@@ -80,7 +80,14 @@ DATABASES = {
          'HOST': 'roundhouse.proxy.rlwy.net',
          'PORT': '11253',
     }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
